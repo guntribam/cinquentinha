5233777151f3d5cfdc0b908c8c0d0b920931db39
Gunter Ribeiro Amorim <gunter.amorim@mpdft.mp.br>
2025-02-05 19:36:09 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ export default async ({ req, res, log, error }) => {
   // 3. Tenta parsear o corpo da requisição (Webhook do Telegram ou CRON)
   let body = {};
   try {
-    body = req.bodyJson
+    body = JSON.parse(req.bodyJson)
     log(body); 
   } catch (err) {
     log('Erro ao fazer JSON.parse do req.payload', err);
